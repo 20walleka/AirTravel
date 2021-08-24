58870c14015595583930a9881d5f4af2937a95fd
Your Name <you@example.com>
2021-08-24 09:57:29 -0500

    PassengerTests.java

--- PassengerTests.java
+++ PassengerTests.java
@@ -14,22 +14,21 @@ import junit.framework.TestCase;
 
 public class PassengerTests extends TestCase 
 {
+    private String wantedString = null;
+    private String lastName = "Smith";
+    private String firstName = "Josh";
+    private String nationality = "Canadian";
+    private String tripOrigin = "Winnipeg";
+    private String destination = "Dublin";
+    private int age = 47; 
+    private Passenger passenger1 = new Passenger("Josh","Smith","Canadian","Winnipeg","Dublin",47);
+    
   /* testToStringMethod - This method
    * will check to make sure the toString
    * method returns the correct result. 
    */
   public void testToStringMethod() 
   {
-    String wantedString = null;
-    String lastName = "Smith";
-    String firstName = "Josh";
-    String nationality = "Canadian";
-    String tripOrigin = "Winnipeg";
-    String destination = "Dublin";
-    int age = 47;
-    
-    Passenger passenger1 = new Passenger("Josh","Smith","Canadian","Winnipeg","Dublin",47);
-    
     wantedString = "Name: "+lastName+", "+firstName+"\n";
     wantedString += "Age: "+age+"\n";
     wantedString += "Nationality of Passenger: "+nationality+"\n";
@@ -38,4 +37,18 @@ public class PassengerTests extends TestCase
 
     assertTrue("The Strings do NOT match",wantedString.equals(passenger1.toString()));
   }//testToStringMethod
+  
+  /* testGetMethods - This method will check 
+   * to make sure the correct information is
+   * returned from getPassengerName, getAge,
+   * getNationality, getTripOrigin, and getDestination.
+   */
+  public void testGetMethods()
+  {
+    assertTrue("Passenger Names do NOT match",(firstName+" "+lastName).equals(passenger1.getPassengerName()));
+    assertEquals("Passenger Ages do NOT match",age,passenger1.getAge());
+    assertTrue("Nationalities do NOT match",nationality.equals(passenger1.getNationality()));    
+    assertTrue("Trip Origins do NOT match",tripOrigin.equals(passenger1.getTripOrigin()));
+    assertTrue("Destinations do NOT match",destination.equals(passenger1.getDestination()));
+  }//testGetMethods
 }//PassengerTests
