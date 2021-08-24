fbd533be575f383ebd409e1ca42a37e46342e484
Your Name <you@example.com>
2021-08-24 10:43:31 -0500

    Airplane.java

--- Airplane.java
+++ Airplane.java
@@ -9,6 +9,21 @@
  * information about an airplane.
  */
 
+//Import needed for  ArrayList use
+import java.util.ArrayList;
+
 public class Airplane
 {
+  //Constant
+  private static final int MAX_WEIGHT = 500000;
+  
+  //Instance Variables
+  private String airline;
+  private int numberPassengers;
+  private int numberCrew;
+  private int cargoWeight;
+  private int kmToDestination;
+  private int ticketCost;
+  private ArrayList<Passenger> peopleOnBoard;
+  
 }//Airplane
