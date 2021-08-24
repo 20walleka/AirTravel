cc9871637bcd34c2bb1b35e94ab4826f8da8efd5
Your Name <you@example.com>
2021-08-24 10:26:29 -0500

    Airplane.java

--- Airplane.java
+++ Airplane.java
@@ -1,3 +1,14 @@
+/*
+ * Airplane
+ * 
+ * Name:    Kate Walley
+ * PROJECT: AirTravel
+ * DATE:    August 24, 2021
+ * 
+ * PURPOSE: This class will hold
+ * information about an airplane.
+ */
+
 public class Airplane
 {
 }//Airplane
