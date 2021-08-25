4d1964d099acac79d327776d0cd3480e1cb4486e
Your Name <you@example.com>
2021-08-25 13:43:37 -0500

    Cargo.java

--- Cargo.java
+++ Cargo.java
@@ -1,3 +1,28 @@
+/*
+ * Cargo
+ * 
+ * Name:    Kate Walley
+ * PROJECT: AirTravel
+ * DATE:    August 24, 2021
+ * 
+ * PURPOSE: This class will hold
+ * information about the Cargo in
+ * the airplane.
+ */
+
 public class Cargo
 {
+  //Instance Variables
+  private String type;
+  private int weight;
+  
+  /* Constructor - This constructor will be passed the parameters
+   * type and weight. These parameters will be used
+   * to set the values of instance variables.
+   */
+  public Cargo(String type, int weight)
+  {
+    this.type = type;
+    this.weight = weight;
+  }//Cargo
 }//Cargo
