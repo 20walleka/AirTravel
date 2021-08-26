20b24e426ea765bab19e8962ea5b44e11e6f7974
Your Name <you@example.com>
2021-08-26 11:28:41 -0500

    Airport.java

--- Airport.java
+++ Airport.java
@@ -9,6 +9,9 @@
  * information about an Airport.
  */
 
+//Import needed for  ArrayList use
+import java.util.ArrayList;
+
 public class Airport
 {
   //Instance Variables
@@ -17,11 +20,23 @@ public class Airport
   private int numberPlanes;
   private int totalPassengers;
   private int totalCrew;
+  private ArrayList<Airplane> planeList;
   
   /* This constructor will assign values to the 
-   * instance variables windSpeed and airportCode.
+   * instance variables windSpeed and airportCode
+   * using the parameters of the same name.
+   * Other variables numberPlanes,totalPassengers,
+   * totalCrew, and planeList will also be set. 
    */
   public Airport(int windSpeed, String airportCode)
   {
+    this.windSpeed = windSpeed;
+    this.airportCode = airportCode;
+    numberPlanes = 0;
+    totalPassengers = 0;
+    totalCrew = 0;
+    planeList = new ArrayList<Airplane>();
   }//Airport
+  
+  
 }//Airport
