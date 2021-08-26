505197bf6d27d523b104f6c2b3f22333c79fdc42
Your Name <you@example.com>
2021-08-26 13:38:02 -0500

    AirportTests.java

--- AirportTests.java
+++ AirportTests.java
@@ -18,6 +18,9 @@ import java.util.ArrayList;
 public class AirportTests extends TestCase 
 {
   private Airport airport1 = new Airport(34,"CYWG");
+  private Airplane plane1 = new Airplane("Air Canada",7,5842,589.40,"Winnipeg","Dublin");
+  private Airplane plane2 = new Airplane("WestJet",8,2048,147,"Winnipeg","Toronto");
+  private Airplane plane3 = new Airplane("Sunwing",10,3253,456,"Winnipeg","Orlando");
   
   /* testGetSetMethods - This method
    * will be used to test getWindSpeed,
@@ -34,4 +37,18 @@ public class AirportTests extends TestCase
     assertEquals("Winds from set method do not match",windSet,airport1.getWindSpeed());
     assertTrue("Airport Codes do not match",airportCodeGet.equals(airport1.getAirportCode()));
   }//testGetSetMethods
+  
+  /*testAddAirplane - This method will test to 
+   * make sure that airplanes are added correctly.
+   * It will also test the printPlaneList as well.
+   */
+  public void testAddAirplane()
+  {
+    String toCompare = "Air Canada From: Winnipeg To: Dublin\nWestJet From: Winnipeg To: Toronto\nSunwing From: Winnipeg To: Orlando\n";
+    System.out.print(toCompare);
+    airport1.addAirplane(plane1);
+    airport1.addAirplane(plane2);
+    airport1.addAirplane(plane3);
+    assertTrue("Added Plane Lists do not match",toCompare.equals(airport1.printPlaneList()));
+  }//testAddAirplane
 }//AirportTests
